b1d169f5d168f6a479d2df99cb80cb5cfefe0ffb
Vincent Rolea <3525369+virolea@users.noreply.github.com>
2024-01-07 18:40:32 +0100

    Bundle css assets with engine

---
+++ app/assets/config/panoptic_manifest.js
@@ -0,0 +1,5 @@
+
+// This tells sprockets to link the build directory
+//= This is how application get loaded through javascript tags
+
+//= link_directory ../stylesheets/panoptic
--- lib/panoptic/engine.rb
+++ lib/panoptic/engine.rb
@@ -5,5 +5,9 @@ require "pagy"
 module Panoptic
   class Engine < ::Rails::Engine
     isolate_namespace Panoptic
+
+    initializer "panoptic.assets.precompile" do |app|
+      app.config.assets.precompile += %w[panoptic/application.css]
+    end
   end
 end
