630c47350c99a85001f732dbeba5473d95841f4d
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-07 12:31:08 +0100

    Order scheduled jobs by scheduled_at: :asc

--- app/controllers/panoptic/jobs/scheduled_controller.rb
+++ app/controllers/panoptic/jobs/scheduled_controller.rb
@@ -7,7 +7,7 @@ module Panoptic
     def index
       # TODO: use the `scheduled` scope available in SolidQueue next release
       @pagy, @jobs = pagy(
-        SolidQueue::Job.joins(:scheduled_execution).order(created_at: :asc)
+        SolidQueue::Job.joins(:scheduled_execution).order(scheduled_at: :asc)
       )
 
       render "panoptic/jobs/index"
