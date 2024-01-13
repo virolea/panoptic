6ef67605e305676b442536deda548a8ea7bc10a3
Vincent Rolea <3525369+virolea@users.noreply.github.com>
2024-01-13 13:03:59 +0100

    Keep assets in same directory Use standard builds/ directory for compiled assets to prevent confusion

--- app/assets/config/panoptic_manifest.js
+++ app/assets/config/panoptic_manifest.js
@@ -2,5 +2,5 @@
 // This tells sprockets to link the build directory
 //= This is how application get loaded through javascript tags
 
-//= link_directory ../stylesheets/panoptic
+//= link_directory ../builds/panoptic
 //= link_tree ../javascripts
