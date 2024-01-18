162ccf273a6f8a555a41cd3349257b6e5302128c
Vincent Rolea <3525369+virolea@users.noreply.github.com>
2024-01-18 19:43:28 +0100

    Remove remaining importmaps code

--- lib/panoptic/engine.rb
+++ lib/panoptic/engine.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "importmap-rails"
 require "turbo-rails"
 require "pagy"
 
@@ -10,12 +9,6 @@ module Panoptic
 
     initializer "panoptic.assets.precompile" do |app|
       app.config.assets.precompile += %w[panoptic_manifest.js]
-    end
-
-    initializer "panoptic.importmap", before: "importmap" do |app|
-      app.config.importmap.paths << Engine.root.join("config/importmap.rb")
-
-      app.config.importmap.cache_sweepers << root.join("app/javascript/panoptic")
     end
   end
 end
